ee8cfe36587f3ad80d70921611d81a64e9f2e7f1
leonorv <leonor.veloso@tecnico.ulisboa.pt>
2020-10-13 19:36:51 +0100

    done(?)

--- js/main.js
+++ js/main.js
@@ -22,11 +22,25 @@ var keys = {
     50: false, //2
     51: false, //3
     52: false, //4
-    69: false, //e
     37: false, //left
     38: false, //top
     39: false, //right
     40: false, //bottom
+}
+
+function addLamps() {
+    father.wires[7].addCylinderLamp(4, 4, 12, 30);
+    father.wires[8].addCylinderLamp(2, 2, 10, 30);
+    father.wires[3].addCubeLamp(5, 5, 5);
+    father.wires[6].addCubeLamp(5, 5, 5);
+    father.wires[4].addCylinderLamp(3, 3, 7, 30);
+    son.wires[3].addCylinderLamp(6, 6, 5, 30);
+    son.wires[2].addCylinderLamp(8, 8, 6, 30);
+    son.wires[5].addCylinderLamp(3, 3, 7, 30);
+    son.wires[6].addCylinderLamp(3, 3, 7, 30);
+    grandson.wires[2].addCubeLamp(5, 5, 5);
+    grandson.wires[3].addCubeLamp(8, 8, 8);
+    grandson.wires[4].addCylinderLamp(3, 3, 7, 30);
 }
 
 function createScene() {
@@ -42,54 +56,60 @@ function createScene() {
     son = new Group();
     grandson = new Group();
 
-    father.addWire(new Wire(0, 10*un, 0, 3*un, 0, 0, 0, scene));
-    father.addWire(new Wire(0, -3*un/2, 0, 10*un, Math.PI/2, 0, 0, scene));
-    father.addWire(new Wire(0, -5*un, un, 2*un, -Math.PI/2, 0, 0, scene));
-    father.addWire(new Wire(0, -(5/2+3/Math.sqrt(32))*un, -un/2, 3/2*un, Math.PI/4, 0, 0, scene));
-    father.addWire(new Wire(0, (5+3/Math.sqrt(32))*un, -un/2, 3/2*un, -Math.PI/4, 0, 0, scene));
-    father.addWire(new Wire(0, -un, 0, 3*un, -Math.PI/2, 0, Math.PI/2, scene));
-    father.addWire(new Wire(un/2, 0, un/2, 3/2*un, 0, -Math.PI/4, -Math.PI/2, scene));
-    father.addWire(new Wire(0, -3/2*un, -3/4*un, 3/2*un, -Math.PI/2, 0, 0, scene));
-    father.addWire(new Wire(0, 3/2*un, -un/2, un, -Math.PI/2, 0, 0, scene));
+    father.wires.push(new Wire(3*un, scene));
+    father.wires[0].setPos(0, 10*un, 0, 0, 0, 0);
+    father.wires.push(new Wire(10*un, scene));
+    father.addWire(0, 1, 0, -father.wires[0].h/2, 0, Math.PI/2, 0, 0);
+    father.wires.push(new Wire(2*un, scene));
+    father.addWire(1, 2, 0, father.wires[1].h/2, father.wires[2].h/2, Math.PI/2, 0, 0);
+    father.wires.push(new Wire(3/2*un, scene));
+    father.addWire(1, 3, 0, (father.wires[1].h/4+(Math.sqrt(2)/4)*father.wires[3].h), -(Math.sqrt(2)/4)*father.wires[3].h, 3*Math.PI/4, 0, 0);
+    father.wires.push(new Wire(3/2*un, scene));
+    father.addWire(1, 4, 0, -(father.wires[1].h/2+(Math.sqrt(2)/4)*father.wires[4].h), -(Math.sqrt(2)/4)*father.wires[4].h, Math.PI/4, 0, 0);
+    father.wires.push(new Wire(3*un, scene));
+    father.addWire(2, 5, 0, father.wires[2].h/2, 0, -Math.PI/2, 0, -Math.PI/2);
+    father.wires.push(new Wire(3/2*un, scene));
+    father.addWire(5, 6, -((Math.sqrt(2)/4)*father.wires[6].h), 0, -((Math.sqrt(2)/4)*father.wires[6].h), Math.PI/2, 0, -Math.PI/4);
+    father.wires.push(new Wire(3/2*un, scene));
+    father.addWire(5, 7, 0, -father.wires[5].h/2, father.wires[7].h/2, -Math.PI/2, 0, 0);
+    father.wires.push(new Wire(un, scene));
+    father.addWire(5, 8, 0, father.wires[5].h/2, father.wires[8].h/2, -Math.PI/2, 0, 0);
 
-    father.addDependencies([[0,1], [1,2], [1,3], [1,4], [2,5], [5,6], [5,7], [5,8]]);
     mobile.addGroup(father);
 
-    son.addWire(new Wire(0, 5/2*un, un, 2*un, -Math.PI/2, 0, 0, scene));
-    son.addWire(new Wire(0, -un, un, 6*un, Math.PI/2, 0, 0, scene));
-    son.addWire(new Wire(0, 3*un, un, 2*un, -Math.PI/2, 0, 0, scene));
-    son.addWire(new Wire(0, 0, 3*un, 6*un, -Math.PI/2, 0, 0, scene));
-    son.addWire(new Wire(0, -3*un, un, 2*un, Math.PI/2, 0, 0, scene));
-    son.addWire(new Wire(-un, -un, 0, 2*un, 0, 0, Math.PI/2, scene));
-    son.addWire(new Wire(0, (3+Math.sqrt(2)/4)*un,-(Math.sqrt(2)/4)*un , un, -Math.PI/4,0, 0, scene));
-    son.addDependencies([[0,1],[1,2],[1,3], [1,4],[0,5],[1,6]]);
+    son.wires.push(new Wire(2*un, scene));
+    son.wires[0].setPos(0, -father.wires[1].h/2, son.wires[0].h/2, -Math.PI/2, 0, 0);
+    son.wires.push(new Wire(6*un, scene));
+    son.addWire(0, 1, 0, -son.wires[0].h/2, 0, Math.PI/2, 0, 0);
+    son.wires.push(new Wire(2*un, scene));
+    son.addWire(1, 2, 0, -son.wires[1].h/2, son.wires[2].h/2, -Math.PI/2, 0, 0);
+    son.wires.push(new Wire(6*un, scene));
+    son.addWire(1, 3, 0, 0, son.wires[3].h/2, -Math.PI/2, 0, 0);
+    son.wires.push(new Wire(2*un, scene));
+    son.addWire(1, 4, 0, son.wires[1].h/2, son.wires[4].h/2, Math.PI/2, 0, 0);
+    son.wires.push(new Wire(2*un, scene));
+    son.addWire(0, 5, son.wires[0].h/2, -son.wires[0].h/2, 0, 0, 0, Math.PI/2);
+    son.wires.push(new Wire(un, scene));
+    son.addWire(1, 6, 0, (son.wires[1]/2+Math.sqrt(2)/4)*son.wires[6].h, -(Math.sqrt(2)/4)*son.wires[6].h, -Math.PI/4, 0, 0);
     
     mobile.addGroup(son);
     mobile.groups[0].wires[1].add(mobile.groups[1].wires[0]);
 
-    grandson.addWire(new Wire(0, 3*un, un, 2*un, Math.PI/2, 0, 0, scene));
-    grandson.addWire(new Wire(0, un, 0, 3*un, -Math.PI/2, 0, 0, scene));
-    grandson.addWire(new Wire(0, -3/2*un, un/2, un, -Math.PI/2, 0, 0, scene));
-    grandson.addWire(new Wire(0, 3/2*un, un/2, un, -Math.PI/2, 0, 0, scene));
-    grandson.addWire(new Wire(un/2, un, 0, un, 0, 0, -Math.PI/2, scene));
-    grandson.addDependencies([[0,1], [1,2], [1,3],[0,4]]);
+    grandson.wires.push(new Wire(2*un, scene));
+    grandson.wires[0].setPos(0, son.wires[1].h/2, grandson.wires[0].h/2, Math.PI/2, 0, 0);
+    grandson.wires.push(new Wire(3*un, scene));
+    grandson.addWire(0, 1, 0, grandson.wires[0].h/2, 0, Math.PI/2, 0, 0);
+    grandson.wires.push(new Wire(un, scene));
+    grandson.addWire(1, 2, 0, -grandson.wires[1].h/2, -grandson.wires[2].h/2, Math.PI/2, 0, 0);
+    grandson.wires.push(new Wire(un, scene));
+    grandson.addWire(1, 3, 0, grandson.wires[1].h/2, -grandson.wires[2].h/2, Math.PI/2, 0, 0);
+    grandson.wires.push(new Wire(un, scene));
+    grandson.addWire(0, 4, grandson.wires[4].h/2, grandson.wires[0].h/2, 0, 0, 0, Math.PI/2);
 
     mobile.addGroup(grandson);
     mobile.groups[1].wires[1].add(mobile.groups[2].wires[0]);
 
-    //LAMPS
-    father.wires[7].addCylinderLamp(3/2*un, 4, 4, 12, 30);
-    father.wires[8].addCylinderLamp(un, 2, 2, 10, 30);
-    father.wires[3].addCubeLamp(-un, 5, 5, 5);
-    father.wires[6].addCubeLamp(un, 5, 5, 5);
-    father.wires[4].addCylinderLamp(un, 3, 3, 7, 30);
-    son.wires[3].addCylinderLamp(-3*un, 6, 6, 5, 30);
-    son.wires[4].addCylinderLamp(un, 8, 8, 6, 30);
-    son.wires[5].addCylinderLamp(un, 3, 3, 7, 30);
-    son.wires[6].addCylinderLamp(un, 3, 3, 7, 30);
-    grandson.wires[2].addCubeLamp(-un, 5, 5, 5);
-    grandson.wires[3].addCubeLamp(-un, 8, 8, 8);
-    grandson.wires[4].addCylinderLamp(un, 3, 3, 7, 30);
+    addLamps();
 }
 
 function createCamera() {
@@ -150,11 +170,6 @@ function onKeyDown(e) {
             break;
         case 52:
             mobile.changeWireframe();
-        case 69:
-            scene.traverse(function (node) {
-                if (node instanceof THREE.AxisHelper)
-                    node.visible = !node.visible;
-            })
             break;
     }
 }
@@ -179,10 +194,15 @@ function keyPressed(delta) {
     if(keys[90]) mobile.groups[2].spinLeft(delta);
     if(keys[67]) mobile.groups[2].spinRight(delta);
 
-    if(keys[37]) mobile.groups[0].moveLeft(delta);
-    if(keys[38]) mobile.groups[0].moveForward(delta);
-    if(keys[39]) mobile.groups[0].moveRight(delta);
-    if(keys[40]) mobile.groups[0].moveBackward(delta);
+    if(keys[37] && keys[38]) mobile.groups[0].moveNorthWest(delta);
+    else if(keys[39] && keys[38]) mobile.groups[0].moveNorthEast(delta);
+    else if(keys[37] && keys[40]) mobile.groups[0].moveSouthWest(delta);
+    else if(keys[39] && keys[40]) mobile.groups[0].moveSouthEast(delta);
+
+    else if(keys[37]) mobile.groups[0].moveLeft(delta);
+    else if(keys[38]) mobile.groups[0].moveForward(delta);
+    else if(keys[39]) mobile.groups[0].moveRight(delta);
+    else if(keys[40]) mobile.groups[0].moveBackward(delta);
 }
 
 function init() {
